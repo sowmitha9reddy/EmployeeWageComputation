a46f5ffcf65d590f61d9c587e3d8a5f886903470
sowmitha9reddy <devireddysowmitha9@gmil.com>
2024-07-08 12:22:09 +0530

    DialyWage

--- Employee.java
+++ Employee.java
@@ -2,22 +2,58 @@ package Employee_Wage;
 
 import java.util.Random;
 
-class Employee 
-{         
-	 public String checkAttendance() {
-		   	
-		   	Random random = new Random();
+class Employee {
+	 static final int WAGE_PER_HOUR = 20;
+	    static final int FULL_DAY_HOURS = 8;
+	    static final int PART_TIME_HOURS = 4;
+	    
+  //  check attendance (using random)
+  public String checkAttendance() {
+  	
+  	Random random = new Random();
 
-				int randomInt = random.nextInt(3); // 0, 1, or 2
+		int randomInt = random.nextInt(3); // 0, 1, or 2
 
-		       switch (randomInt) {
-		           case 0:
-		               return "FullTime";
-		           case 1:
-		               return "PartTime";
-		           default:
-		               return "Absent";
-		       }
-}
+      switch (randomInt) {
+          case 0:
+              return "FullTime";
+          case 1:
+              return "PartTime";
+          default:
+              return "Absent";
+      }
+
+  }
+ 
+      
+      public void computeWage() {
+          // Check if employee is full-time, part-time or absent
+          String attendance = checkAttendance();
+          
+switch (attendance) {
+              case "FullTime":
+                  System.out.println("Employee is FullTime");
+                 
+                  int dailyWageFullTime = WAGE_PER_HOUR *FULL_DAY_HOURS ;
+                  System.out.println("Daily Wage: $" + dailyWageFullTime);
+                  break;
+              case "PartTime":
+                  System.out.println("Employee is PartTime");
+                  // Calculate wage for part-time hours
+                  int dailyWagePartTime = WAGE_PER_HOUR * PART_TIME_HOURS;
+                  System.out.println("Daily Wage: $" + dailyWagePartTime);
+                  break;
+             
+                  
+                  
+              default:
+              	System.out.println("Employee is Absent");
+                  System.out.println("No wage computation.");
+            
+             }
+      }
+
+      
+ }
 
 
--- EmployeeWageComputationSystemProblem.java
+++ EmployeeWageComputationSystemProblem.java
@@ -5,7 +5,8 @@ import java.util.Scanner;
 public class EmployeeWageComputationSystemProblem {
 	public static void main(String[] args) {
 		       System.out.println("Welcome to Employee Wage Computation");
-	        
+		       Employee employ = new Employee();
+		       employ.computeWage();
 	}
 
 }
