1aebc8c87ca93e96d6b72138fee08ce46b0e642f
sowmitha9reddy <106576086+sowmitha9reddy@users.noreply.github.com>
2024-07-08 12:32:42 +0530

    Update EmployeeWageComputationSystem.java

--- src/Employee_Wage/EmployeeWageComputationSystem.java
+++ src/Employee_Wage/EmployeeWageComputationSystem.java
@@ -1,63 +1,29 @@
 package Employee_Wage;
-import java.util.Random;
+
+public static void main(String[] args) {
+		       System.out.println("Welcome to Employee Wage Computation");
+		       Scanner scan=new Scanner(System.in);
+		       Employee employ = new Employee();
+		       System.out.println("Enter the no.of Companies");
+		       int n=scan.nextInt();
+		        for(int i=0;i<n;i++) {
+		        System.out.println("Enter the details of Company(Company Name,Wage per Hour,Maximum Working Days,Maximum Working Hours)Ex:(companyA,20,20,100))");
+		        String s=scan.next();
+		        String[] st=s.split(",");
+		        int mWD=Integer.parseInt(st[2]);
+		        String cName=st[0];
+		        int wPH=Integer.parseInt(st[1]);
+		        int mWH=Integer.parseInt(st[3]);
+		        System.out.println("Employ of"+cName);
+		        employ.computeMonthlyWage(wPH,mWD,mWH);
+		        System.out.println("----------");
+		        }
+
+	}
 
 
 
 
-
-class Employee {
-	 static final int WAGE_PER_HOUR = 20;
-	    static final int FULL_DAY_HOURS = 8;
-	    static final int PART_TIME_HOURS = 4;
-	    
-   //  check attendance (using random)
-   public String checkAttendance() {
-   	
-   	Random random = new Random();
-
-		int randomInt = random.nextInt(3); // 0, 1, or 2
-
-       switch (randomInt) {
-           case 0:
-               return "FullTime";
-           case 1:
-               return "PartTime";
-           default:
-               return "Absent";
-       }
-
-   }
-  
-       
-       public void computeWage() {
-           // Check if employee is full-time, part-time or absent
-           String attendance = checkAttendance();
-           
-switch (attendance) {
-               case "FullTime":
-                   System.out.println("Employee is FullTime");
-                  
-                   int dailyWageFullTime = WAGE_PER_HOUR *FULL_DAY_HOURS ;
-                   System.out.println("Daily Wage: $" + dailyWageFullTime);
-                   break;
-               case "PartTime":
-                   System.out.println("Employee is PartTime");
-                   // Calculate wage for part-time hours
-                   int dailyWagePartTime = WAGE_PER_HOUR * PART_TIME_HOURS;
-                   System.out.println("Daily Wage: $" + dailyWagePartTime);
-                   break;
-              
-                   
-                   
-               default:
-               	System.out.println("Employee is Absent");
-                   System.out.println("No wage computation.");
-             
-              }
-       }
-
-       
-  }
 
 
   
@@ -69,14 +35,3 @@ switch (attendance) {
 	
 
 
-public class EmployeeWageComputationSystem {
-	public static void main(String[] args) {
-	       System.out.println("Welcome to Employee Wage Computation");
-
-	       // Create an instance of EmployeeAttendanceChecker
-	       Employee employ = new Employee();
-
-	       employ.computeWage();
-	   }
-
-}
